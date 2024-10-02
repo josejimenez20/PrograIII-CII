e3a5baaf6d09729b292f811b5566213e9584f311
JoseJimenez20w <141762910+josejimenez20@users.noreply.github.com>
2024-10-02 07:29:35 -0600

    Hola Bienevenido

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', event => {
-    mnxAcademica.addEventListener('click', e => {
-        e.preventDefault();
-        let form = e.target.dataset.form;
-        console.log(form);
-    });
-});
