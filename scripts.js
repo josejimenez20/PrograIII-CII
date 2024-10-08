129c74c2d67d3884c9a861e3d0a819fa6f575c08
JoseJimenez20w <141762910+josejimenez20@users.noreply.github.com>
2024-10-08 15:18:55 -0600

    UNION DE ARCHIVO DE REGISTRO

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function showRegister() {
-    document.getElementById('login-form').classList.add('hidden');
-    document.getElementById('register-form').classList.remove('hidden');
-}
-
-function showLogin() {
-    document.getElementById('register-form').classList.add('hidden');
-    document.getElementById('login-form').classList.remove('hidden');
-}
